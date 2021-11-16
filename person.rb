2536366f4802224de91229d49e689f6a9e629833
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-11-16 12:13:50 -0500

    Add person class

---
+++ person.rb
@@ -0,0 +1,31 @@
+class Person
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, name: 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  private
+
+  def of_age?
+    return true if @age >= 18
+    false
+  end
+
+  public
+
+  def can_use_services?
+    true if @age >= 18 || @parent_permission
+  end
+
+  def check_age?
+    of_age?
+  end 
+end
+
+person1 = Person.new(17)
+puts person1.check_age?
