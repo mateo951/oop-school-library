2a0f119ad673ffd35fb2b0db7d0271e6c4593498
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-11-16 22:39:41 -0500

    Add corrector functionality

--- person.rb
+++ person.rb
@@ -1,3 +1,5 @@
+require './corrector'
+
 class Person
   attr_accessor :name, :age
   attr_reader :id
@@ -7,6 +9,7 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @corrector = Corrector.new
   end
 
   private
@@ -26,4 +29,9 @@ class Person
   def check_age?
     of_age?
   end
+
+  def validate_name
+    @name = @corrector.correct_name(@name)
+  end
+
 end
