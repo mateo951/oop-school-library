282bb2ce0cfc7e1e099ee779ad9ec109b64b12a7
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-11-18 13:04:48 -0500

    Add book class

---
+++ book.rb
@@ -0,0 +1,10 @@
+class Book 
+  attr_accessor :title, :author
+  def initialize(title:, author:)
+    @title = title
+    @author = author
+  end
+end
+
+book1 = Book.new(title: 'Harry', author: 'JK')
+puts book1.title
